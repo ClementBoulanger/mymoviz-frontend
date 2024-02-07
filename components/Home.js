c0003f7e88a390f2551fe4de3f4c7f3b4fd30ece
Clément Boulanger <clement.boulangerpro@gmail.com>
2024-02-07 14:53:22 +0100

    fetch vercel modifié 2

--- components/Home.js
+++ components/Home.js
@@ -44,7 +44,7 @@ function Home() {
   ];
 */
   useEffect(() => {
-    fetch('https://mymoviz-backend-zeta-one.vercel.app/routes/index')
+    fetch('https://mymoviz-backend-zeta-one.vercel.app/routes/index/movies')
       .then(response => response.json())
       .then(data => {
         const formatedData = data.movies.map(movie => {
